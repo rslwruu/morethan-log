509fd533ccff2118e24af2498bef9bc6e7e01a34
dlwhdqh_ <lrhkuunp@yonsei.ac.kr>
2024-10-22 01:36:30 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   blog: {
     title: "dlwhdqh Archive",
     description: "...",
-    scheme: "light", // 'light' | 'dark' | 'system'
+    scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
