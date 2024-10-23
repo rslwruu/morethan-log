59128d3bd182509ecd1ed966a5d908cd653d0367
dlwhdqh_ <lrhkuunp@yonsei.ac.kr>
2024-10-23 15:13:17 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "Junseo",
+    name: "JUNSEOOOO",
     image: "/goto.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "CSE 24",
-    bio: "이른 아침에 감자 많이 먹어라",
+    bio: "나한테명령하지마명령할거면마인크래프트명령어로해",
     email: "lrhkuunp@yonsei.ac.kr",
     linkedin: "",
     github: "rslwruu",
