c1a24eff8b2be93857a373e01be07c47f8c85b26
dlwhdqh_ <lrhkuunp@yonsei.ac.kr>
2024-10-25 20:24:11 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/rslwruu/morethan-log",
+      name: ``,
+      href: "",
     },
   ],
   // blog setting (required)
