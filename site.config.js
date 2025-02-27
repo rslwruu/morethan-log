d2433e3edb237f8d3f9a6eda7f2fd07a22d6ba9d
dlwhdqh_ <lrhkuunp@yonsei.ac.kr>
2025-02-28 07:42:18 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "unwecfrte",
+    name: "!snotanoption",
     image: "/profile.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "CSE 24",
     bio: "",
